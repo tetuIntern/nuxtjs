ba6060e55c5893b2a9eb8bca6d77ab2a98d60ff5
p-tetsunosuke.shimoda@freegian.co.jp <tetuprogram@yahoo.co.jp>
2021-06-26 16:49:46 +0900

    remake

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav-bar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+export default {
+  components:{
+    NavBar,
+  }
+}
+</script>
